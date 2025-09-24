1f04ae7fc4c9c13dbedcfb8300c7804379fa84f7
Acruxu <giuliodori.juancruz03@gmail.com>
2025-09-24 18:14:57 -0300

    primer commit

--- models/products.js
+++ models/products.js
@@ -3,6 +3,19 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   name: { type: String },
+  team: { type: String },
+  driver: { type: String },
+  price: {type: String},
+  stock: {type: String},
+  colors: [{ type: String }],
+  description:{type:String},
+  sizes: [{ type: String }],
+  details:[{ type: String }],
+  categories: [{type: Schema.Types.ObjectId,ref: "Category"}]
 });
 
 export default mongoose.model("Product", productSchema, "Products");
+
+
+
+
--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,74 @@ const router = express.Router();
 import Product from "../models/products.js";
 
 const findAllProducts = async (req, res) => {
-
+    try {
+        const products = await Product.find().select("_id team driver price stock colors description sizes details categories")
+        return res.status(200).send({message: "todos los products", products: products})
+    } catch(error) {
+        return res.status(501).send({message: "hubo un error", error})
+    }
 };
+
+const findOneProduct = async (req, res) => {
+    const {id} = req.params
+    try {
+        const product = await Product.findOne({_id: id}).select("_id team driver price stock colors description sizes details categories")
+        return res.status(200).send({message: "Producto encontrado", product: product})
+    } catch (error) {
+        return res.status(501).send({message: "hubo un error", error})
+    }
+}
+
+const addProduct = async (req, res) => {
+    
+    try {
+        const product = new Product(req.body)
+        await product.save()
+         return res.status(200).send({message: "Producto creado", product: product})
+    } catch (error) {
+        return res.status(501).send({message: "hubo un error", error})
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    const {id} = req.params
+    try {
+        const productToDelete = await Product.findOne({_id: id})
+    if(!productToDelete){
+        return res.status(404).send({message: "No existe el product", id: id})
+    }
+        await Product.deleteOne({_id: id})
+        return res.status(200).send({message: "Producto Borrado", product: productToDelete})
+    } catch (error) {
+        return res.status(501).send({message: "hubo un error", error})
+    }
+}
+
+const updateProduct = async (req, res) => {
+    const {id} = req.params
+    const {name} = req.body
+    try {
+        const productToUpdate = await Product.findOne({_id: id})
+    if(!productToUpdate){
+        return res.status(404).send({message: "No existe el product", id: id})
+    }
+
+    //valores a actualizar
+    productToUpdate.name = name
+
+        await productToUpdate.save()
+        return res.status(200).send({message: "Producto actualizado", product: productToUpdate})
+    } catch (error) {
+        return res.status(501).send({message: "hubo un error", error})
+    }
+}
+
 
 
 //CRUD endpoints
 router.get("/", findAllProducts);
-
+router.get("/:id", findOneProduct);
+router.post("/", addProduct)
+router.delete("/:id",deleteProduct)
+router.put("/:id",updateProduct)
 export default router;
